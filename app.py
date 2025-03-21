3a0da12cb5fe79d7fcb0fe768248c644b6b92164
Austin Wade Smith <newcubes@gmail.com>
2025-03-21 14:35:32 -0400

    Resolve merge conflict in <file-name>

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 from flask import Flask, jsonify
-from local_sense import LocalSense
-from celestial_sense import CelestialSense
+import sqlite3
+
+DB_PATH = 'sense_data.db'
 
 app = Flask(__name__)
+
+def fetch_latest_data():
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM SensorData ORDER BY timestamp DESC LIMIT 1')
+        row = cursor.fetchone()
+        if row:
+            return {
+                'id': row[0],
+                'timestamp': row[1],
+                'local_data': row[2],
+                'celestial_data': row[3]
+            }
+        return None
 
 @app.route('/')
 def home():
     return "Welcome to the Sense Data App!"
 
-@app.route('/local')
-def local_data():
-    local_sense = LocalSense()
-    data = local_sense.get_data()
-    return jsonify(data)
-
-@app.route('/celestial')
-def celestial_data():
-    celestial_sense = CelestialSense()
-    data = celestial_sense.get_data()
-    return jsonify(data)
+@app.route('/latest')
+def latest_data():
+    data = fetch_latest_data()
+    if data:
+        return jsonify(data)
+    else:
+        return jsonify({'error': 'No data available'}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000) 
